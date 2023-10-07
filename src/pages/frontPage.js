1ac159094013be2fdfe43d1f696fac2628dfb8c1
HuangBugWei <bugwei@caece.net>
2023-10-07 14:41:43 +0800

    basic frontpage layout

--- src/pages/frontPage.js
+++ src/pages/frontPage.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
+import styled from 'styled-components';
+import ForceGraph from 'react-force-graph-3d';
+
 
 function Copyright() {
   return (
@@ -22,6 +24,17 @@ function Copyright() {
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
+const N = 300;
+const myData = {
+  nodes: [...Array(N).keys()].map(i => ({ id: i })),
+  links: [...Array(N).keys()]
+    .filter(id => id)
+    .map(id => ({
+      source: id,
+      target: Math.round(Math.random() * (id-1))
+    }))
+};
+
 export default function FrontPage() {
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -32,24 +45,96 @@ export default function FrontPage() {
           minHeight: '100vh',
         }}
       >
-        <CssBaseline />
-        <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
-          <Typography variant="h2" component="h1" gutterBottom>
-            What's new
-          </Typography>
-          <Typography variant="h1" component="h1" gutterBottom>
-            in Open Science?
-          </Typography>
-          <Typography variant="h5" component="h2" gutterBottom>
-            {'Pin a footer to the bottom of the viewport.'}
-            {'The footer will move as the main element of the page grows.'}
-          </Typography>
-          <Typography variant="body1">Sticky footer placeholder.</Typography>
-        </Container>
+        <Box
+          component="content"
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            backgroundColor: 'blue',
+            flexGrow: 1 // auto fill the remain
+          }}
+        >
+          <Box
+            component="leftinfo"
+            sx={{
+              flex: 3,
+              display: 'flex',
+              flexDirection: 'column',
+              backgroundColor: 'orange',
+              alignItems: 'end'
+            }}
+          >
+            <Box
+              sx={{
+                flex: 3,
+                width: '90%',
+                display: 'flex',
+                flexDirection: 'column',
+                backgroundColor: 'black',
+                justifyContent: 'flex-start',
+              }}
+            >
+              <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                backgroundColor: 'yellow',
+                flexGrow: 1
+              }}
+            ></Box>
+              <Typography variant="h1"
+                sx={{
+                  color: 'white', 
+                  fontSize: '600%',
+                  fontFamily: 'sans-serif',
+                  fontStyle: 'bold'
+                }}
+              >What's New</Typography>
+              <Typography variant="h1" gutterBottom
+                sx={{
+                  color: 'white', 
+                  fontSize: '500%',
+                  fontFamily: 'sans-serif',
+                  fontStyle: 'bold'
+                }}
+              >in Open Science</Typography>
+            </Box>
+            <Box
+              sx={{
+                flex: 2,
+                width: '90%',
+                display: 'flex',
+                flexDirection: 'column',
+                backgroundColor: 'white',
+              }}
+            >
+              <Typography variant="h5" component="h2" gutterBottom>
+              {'Pin a footer to the bottom of the viewport.'}
+              {'The footer will move as the main element of the page grows.'}
+              </Typography>
+              <Typography variant="body1">Sticky footer placeholder.</Typography>
+            </Box>
+          </Box>
+          <Box
+            component="rightinfo"
+            sx={{
+              flex: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              backgroundColor: 'green',
+              justifyContent: 'center',
+              alignItems: 'start'
+            }}
+          >
+            <ForceGraph graphData={myData} 
+            width={800}
+            height={500}/>
+          </Box>
+        </Box>
         <Box
           component="footer"
           sx={{
-            py: 3,
+            py: 2,
             px: 2,
             mt: 'auto',
             backgroundColor: (theme) =>
